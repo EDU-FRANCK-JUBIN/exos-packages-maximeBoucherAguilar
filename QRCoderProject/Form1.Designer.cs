36b676415b57815c7202771ccf25ccf6d869053b
Maxime BOUCHER-AGUILAR <m.boucheraguilar@campus-igs-toulouse.fr>
2019-12-18 15:14:57 +0100

    Ajoutez des fichiers projet.

---
+++ QRCoderProject/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace QRCoderProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.bt_generer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(50, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(599, 374);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // bt_generer
+            // 
+            this.bt_generer.Location = new System.Drawing.Point(685, 38);
+            this.bt_generer.Name = "bt_generer";
+            this.bt_generer.Size = new System.Drawing.Size(75, 23);
+            this.bt_generer.TabIndex = 1;
+            this.bt_generer.Text = "Générer";
+            this.bt_generer.UseVisualStyleBackColor = true;
+            this.bt_generer.Click += new System.EventHandler(this.bt_generer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_generer);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Fenêtre 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button bt_generer;
+    }
+}
+
---
+++ QRCoderProject/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using QRCoderProject;
+using iText;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace QRCoderProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+/*            QRCodeGenerator monQrCode = new QRCodeGenerator();
+            QRCodeData qRCodeData = monQrCode.CreateQrCode("http://www.facebook.com", QRCodeGenerator.ECCLevel.Q);
+            QRCode qRCode = new QRCode(qRCodeData);
+            Bitmap monImage = qRCode.GetGraphic(10, Color.Red, Color.Blue, true);
+            PictureBox maPictureBox = new PictureBox();
+            maPictureBox.Width = 800;
+            maPictureBox.Height = 800;
+            maPictureBox.Image = monImage;
+            this.Controls.Add(maPictureBox);*/
+        }
+
+        private void bt_generer_Click(object sender, EventArgs e)
+        {
+            string monTexte = richTextBox1.Text;
+            var writer = new PdfWriter(@"c:\Temp\Test.pdf");
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+            document.Add(new Paragraph(monTexte));
+            document.Close();
+        }
+    }
+}
